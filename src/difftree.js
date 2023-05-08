491ae93c8881890c3412de9f02970f6dd01e81cb
KBelonozhko <belonozhecka@gmail.com>
2023-05-08 12:10:10 +0300

    fix difftree.js and stylish.js

--- src/difftree.js
+++ src/difftree.js
@@ -6,7 +6,9 @@ const getDifferenceTrees = (dataObj1, dataObj2) => {
   const keys = _.sortBy(_.union(keys1, keys2));
 
   const diffTrees = keys.map((key) => {
-    if (!Object.hasOwn(dataObj1, key)) {
+    if (_.isObject(dataObj1[key]) && _.isObject(dataObj2[key])) {
+      return { key, children: getDifferenceTrees(dataObj1[key], dataObj2[key]), type: 'nested' };
+    } if (!Object.hasOwn(dataObj1, key)) {
       return { key, value: dataObj2[key], type: 'added' };
     } if (!Object.hasOwn(dataObj2, key)) {
       return { key, value: dataObj1[key], type: 'deleted' };
@@ -14,13 +16,10 @@ const getDifferenceTrees = (dataObj1, dataObj2) => {
       return {
         key, value1: dataObj1[key], value2: dataObj2[key], type: 'changed',
       };
-    } if (_.isObject(dataObj1[key]) && _.isObject(dataObj2[key])) {
-      return { key, children: getDifferenceTrees(dataObj1[key], dataObj2[key]), type: 'nested' };
     }
     return { key, value: dataObj2[key], type: 'unchanged' };
   });
 
-  console.log(diffTrees);
   return diffTrees;
 };
 
--- src/stylish.js
+++ src/stylish.js
@@ -1,41 +1,46 @@
 import _ from 'lodash';
 
-const getIndent = (depth, replacer = ' ', spaceCount = 4) => replacer.repeat(depth * spaceCount - 2);
-const getBackIndent = (depth, replacer = ' ', spaceCount = 4) => replacer.repeat(depth * spaceCount);
+const replacer = ' ';
+const getIndent = (depth, spaceCount = 4) => replacer.repeat(depth * spaceCount - 2);
+const getBackIndent = (depth, spaceCount = 4) => replacer.repeat(depth * spaceCount);
 
-const stringfy = (arr, depth = 1) => {
+const stringfy = (arr, depth) => {
   if (!_.isObject(arr)) {
     return String(arr);
   }
   const lines = Object
     .entries(arr)
-    .map(([key, val]) => `${getIndent(depth + 1)}  ${key}: ${stringfy(val, depth + 1)}`);
+    .map(([key, val]) => `${getIndent(depth)}  ${key}: ${stringfy(val, depth + 1)}`);
 
-  return `{\n${lines.join('\n')}\n  ${getBackIndent(depth)}}`;
+  return ['{', ...lines, `${getBackIndent(depth - 1)}}`].join('\n');
 };
 
-const stylish = (node, depth) => {
-  const nodeLines = node.map((item) => {
-    const {
-      key, value, type,
-    } = item;
-    switch (type) {
-      case 'added':
-        return `${getIndent(depth)}+ ${key}: ${stringfy(value, depth + 1)}`;
-      case 'deleted':
-        return `${getIndent(depth)}- ${key}: ${stringfy(value, depth + 1)}`;
-      case 'changed':
-        return `${getIndent(depth)}- ${key}: ${stringfy(item.value1, depth + 1)}\n${getIndent(depth)}+ ${key}: ${stringfy(item.value2, depth + 1)}`;
-      case 'nested':
-        return `${getIndent(depth)}  ${key}: ${stylish(item.children, depth + 1)}`;
-      case 'unchanged':
-        return `${getIndent(depth)}  ${key}: ${stringfy(value, depth + 1)}`;
-      default:
-        throw new Error('Wrong type');
-    }
-  });
+const stylish = (node) => {
+  const iter = (currentNode, depth) => {
+    const nodeLines = currentNode.map((item) => {
+      const {
+        key, value, type,
+      } = item;
+      switch (type) {
+        case 'nested':
+          return `${getIndent(depth)}  ${key}: {\n${iter(item.children, depth + 1)}\n${getBackIndent(depth)}}`;
+        case 'added':
+          return `${getIndent(depth)}+ ${key}: ${stringfy(value, depth + 1)}`;
+        case 'deleted':
+          return `${getIndent(depth)}- ${key}: ${stringfy(value, depth + 1)}`;
+        case 'changed':
+          return `${getIndent(depth)}- ${key}: ${stringfy(item.value1, depth + 1)}\n${getIndent(depth)}+ ${key}: ${stringfy(item.value2, depth + 1)}`;
+        case 'unchanged':
+          return `${getIndent(depth)}  ${key}: ${stringfy(value, depth + 1)}`;
+        default:
+          throw new Error('Wrong type');
+      }
+    });
 
-  return `{\n${nodeLines.join('\n')}\n}`;
+    return nodeLines.join('\n');
+  };
+
+  return `{\n${iter(node, 1)}\n}`;
 };
 
 export default stylish;
