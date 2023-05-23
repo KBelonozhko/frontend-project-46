fd4c750ce5650fc770728bcebb956ab8d58f8e35
KBelonozhko <belonozhecka@gmail.com>
2023-05-23 13:41:33 +0300

    feature JSON comparison

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('JSON comparison', () => {
+test('json comparison', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('testFile.txt'));
 });
 
@@ -18,7 +18,7 @@ test('yml comparison', () => {
   expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(readFile('testFile.txt'));
 });
 
-test('JSON comparison', () => {
+test('json comparison', () => {
   expect(genDiff(getFixturePath('filenested1.json'), getFixturePath('filenested2.json'))).toEqual(readFile('testNestedFile.txt'));
 });
 
@@ -32,4 +32,12 @@ test('plain yaml comparison', () => {
 
 test('plain json comparison', () => {
   expect(genDiff(getFixturePath('filenested1.json'), getFixturePath('filenested2.json'), 'plain')).toEqual(readFile('testPlainFile.txt'));
+});
+
+test('JSON comparison', () => {
+  expect(genDiff(getFixturePath('filenested1.json'), getFixturePath('filenested2.json'), 'json')).toEqual(readFile('testJSONFile.txt'));
+});
+
+test('JSON comparison', () => {
+  expect(genDiff(getFixturePath('filenested1.yaml'), getFixturePath('filenested2.yaml'), 'json')).toEqual(readFile('testJSONFile.txt'));
 });
