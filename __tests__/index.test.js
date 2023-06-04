9e32441964cfdb97fa415827423b2e350fac7eb1
KBelonozhko <belonozhecka@gmail.com>
2023-06-04 10:57:28 +0300

    refactor index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,26 +10,13 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('json comparison', () => {
-  expect(genDiff(getFixturePath('filenested1.json'), getFixturePath('filenested2.json'))).toEqual(readFile('testNestedFile.txt'));
-});
-
-test('yaml comparison', () => {
-  expect(genDiff(getFixturePath('filenested1.yaml'), getFixturePath('filenested2.yaml'))).toEqual(readFile('testNestedFile.txt'));
-});
-
-test('plain yaml comparison', () => {
-  expect(genDiff(getFixturePath('filenested1.yaml'), getFixturePath('filenested2.yaml'), 'plain')).toEqual(readFile('testPlainFile.txt'));
-});
-
-test('plain json comparison', () => {
-  expect(genDiff(getFixturePath('filenested1.json'), getFixturePath('filenested2.json'), 'plain')).toEqual(readFile('testPlainFile.txt'));
-});
-
-test('JSON comparison', () => {
-  expect(genDiff(getFixturePath('filenested1.json'), getFixturePath('filenested2.json'), 'json')).toEqual(readFile('testJSONFile.txt'));
-});
-
-test('JSON comparison', () => {
-  expect(genDiff(getFixturePath('filenested1.yaml'), getFixturePath('filenested2.yaml'), 'json')).toEqual(readFile('testJSONFile.txt'));
+test.each([
+  ['filenested1.json', 'filenested2.json', 'testNestedFile.txt', 'stylish'],
+  ['filenested1.yaml', 'filenested2.yaml', 'testNestedFile.txt', 'stylish'],
+  ['filenested1.json', 'filenested2.json', 'testPlainFile.txt', 'plain'],
+  ['filenested1.yaml', 'filenested2.yaml', 'testPlainFile.txt', 'plain'],
+  ['filenested1.json', 'filenested2.json', 'testJSONFile.txt', 'json'],
+  ['filenested1.yaml', 'filenested2.yaml', 'testJSONFile.txt', 'json'],
+])('test formatters comparison', (file1, file2, testFile, format) => {
+  expect(genDiff(getFixturePath(file1), getFixturePath(file2), format)).toEqual(readFile(testFile));
 });
